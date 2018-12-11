902e9b54c7413a00aa2823a07d2a3201e2a9f2d1
Alessandro <a.fontanella@studenti.unitn.it>
2018-12-11 15:25:33 +0100

    Add function string square

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ var courses_offered = [{id: 21, name: 'HCI'},
  
 app.get('/', (req, res) => res.send('Hello World!'))
  
-app.get('/courses', (req, res) => {
-   res.json(courses_offered)
-})
+app.get('/square', (req, res) => {
+   res.json([{result: string_square(req.query.string)}])
+});
  
 app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
+
+function string_square(s){
+    if(typeof s === 'string') {
+        var returnObj = (s.length)*(s.length);
+    } else {
+        var returnObj = -1;
+    }
+    return returnObj;
+}
