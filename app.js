97a0598022278775e2bd40d71194f007b8d9490a
Alessandro <a.fontanella@studenti.unitn.it>
2018-12-12 13:56:41 +0100

    Correzione app.js

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
  
-var courses_offered = [{id: 21, name: 'HCI'},
-{id: 28, name:'sweng'}]
- 
- 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Insert /square?string=my_string'))
  
 app.get('/square', (req, res) => {
    res.json([{result: string_square(req.query.string)}])
